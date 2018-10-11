8918a44f389e6fd2eabf4b223d148242939e3ffa
Justin Newby <jnewby91@gmail.com>
2018-10-11 11:09:30 -0400

    Adds functions to APP.JS for Logging into Site and Creating New User

--- public/app.js
+++ public/app.js
@@ -1,59 +1,57 @@
+let MOCK_DATA = {
+    "notes": [{
+            "id": 1111111,
+            "userName": "jnewby",
+            "topic": "God's Love for His Children",
+            "chapterVerse": "John 3:16",
+            "noteType": "personal",
+            "visibility": "public",
+            "dateCreated": "01301993",
+            "reflection": "Jesus died for our sins, so we would have grace from God God loved us so much he Jesus to carry out this plan"
 
-let MOCK_DATA = {   
-    "notes": [
-    {
-	"id": 1111111,
-	"userName": "jnewby",
-	"topic": "God's Love for His Children",
-	"chapterVerse": "John 3:16",
-    "noteType": "personal",
-    "visibility": "public",
-	"dateCreated":"01301993",
-	"reflection": "Jesus died for our sins, so we would have grace from God God loved us so much he Jesus to carry out this plan"
-    
-}, 	
-{
-    "id": 2222222,
-    "userName": "jnewby",
-    "topic": "God's Will Never Forsake Us",
-    "chapterVerse": "Psalm 37:25",
-    "noteType": "testimonial",
-    "visibility": "private",
-    "dateCreated":"08122018",
-    "reflection": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
- }, 
- {
-    "id": 3333333,
-    "userName": "jnewby",
-    "topic": "What God's Intentions Are" ,
-    "chapterVerse": "John 10:11",
-    "noteType": "pastor",
-    "visibilty": "public",
-    "dateCreated":"08302018",
-    "reflection": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
- },
- {
-    "id": 4444444,
-    "userName": "jnewby",
-    "topic": "Be at Peace" ,
-    "chapterVerse": "Phillippians 4:6",
-    "noteType": "personal",
-    "visibilty": "public",
-    "dateCreated":"09152018",
-    "reflection": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
- }
-]
+        },
+        {
+            "id": 2222222,
+            "userName": "jnewby",
+            "topic": "God's Will Never Forsake Us",
+            "chapterVerse": "Psalm 37:25",
+            "noteType": "testimonial",
+            "visibility": "private",
+            "dateCreated": "08122018",
+            "reflection": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        },
+        {
+            "id": 3333333,
+            "userName": "jnewby",
+            "topic": "What God's Intentions Are",
+            "chapterVerse": "John 10:11",
+            "noteType": "pastor",
+            "visibilty": "public",
+            "dateCreated": "08302018",
+            "reflection": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        },
+        {
+            "id": 4444444,
+            "userName": "jnewby",
+            "topic": "Be at Peace",
+            "chapterVerse": "Phillippians 4:6",
+            "noteType": "personal",
+            "visibilty": "public",
+            "dateCreated": "09152018",
+            "reflection": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        }
+    ]
 };
 
 
-const NOTES_URL = 'http://localhost:8080/notes'; 
+const NOTES_URL = 'http://localhost:8080/notes';
 const USERS_URL = 'http://localhost:8080/users';
-const AUTH_URL =  'http://localhost:8080/auth/login';
+const AUTH_URL = 'http://localhost:8080/auth/login';
 
 
-function getNotes(callbackFn){
-    setTimeout(function (){
-        callbackFn(MOCK_DATA) 
+function getNotes(callbackFn) {
+    setTimeout(function () {
+        callbackFn(MOCK_DATA)
     }, 100);
 }
 
@@ -64,48 +62,126 @@ function getNotes(callbackFn){
 //  2. Use variables to create an AJAX call to authentication URL
 //  3.
 
-
-
-function logintoSite(){
-    $('.sign_in form').submit(function(){
+function logintoSite() {
+    $('.sign_in form').submit(function () {
         console.log('I hear you');
         event.preventDefault();
         const user = $('form').find('#signIn_userName').val();
-        const pass=$('form').find('#signIn_password').val();
-        console.log(user);
-        console.log(pass);
+        const pass = $('form').find('#signIn_password').val();
         // $.post(AUTH_URL, JSON.stringify({username: 'jnewby12', password: 'something'}), function(data){
         //     console.log(data);
         // }
 
-            $.ajax({
+        $.ajax({
             dataType: 'json',
             method: "POST",
             url: AUTH_URL,
-            contentType: "application/json", 
-            data: JSON.stringify({username: user, password: pass }),
-            error: function(a, b, c){
-                console.log(a,b,c)
+            contentType: "application/json",
+            data: JSON.stringify({
+                username: user,
+                password: pass
+            }),
+            error: function (a, b, c) {
+                console.log(a, b, c)
             },
-            success: function(data){
+            success: function (data) {
                 console.log(data);
                 localStorage.setItem('jwtToken', data.jwtToken);
+                $('html').load('./my_notes.html');
             }
         })
     });
 }
 
-function getInputValue(destination){
+function signupForSite() {
+    $('.sign_up form').submit(function () {
+        event.preventDefault();
+        const email = $('form').find('#signUpEmail').val();
+        const username = $('form').find('#userName').val();
+        const fName = $('form').find('#signUpFirstName').val();
+        const lName = $('form').find('#signUpLastName').val();
+        const password = $('form').find('#signUpPassword').val();
+        console.log(email);
+        console.log(username);
+
+        $.ajax({
+            dataType: 'json',
+            method: "POST",
+            url: USERS_URL,
+            contentType: "application/json",
+            data: JSON.stringify({
+                email: email,
+                firstName: fName,
+                lastname: lName,
+                userName: username,
+                password: password,
+            }),
+            error: function (a, b, c) {
+                console.log(a, b, c)
+            },
+            success: function (data) {
+                console.log(data);
+                localStorage.setItem('jwtToken', data.jwtToken);
+                $('html').load('./my_notes.html');
+            }
+        })
+
+    });
+
+}
+
+function createNotes() {
+    $('.note_Form form').submit(function () {
+        const topic = $('form').find('#topic').val();
+        const book = $('form').find('#book').val();
+        const chapter = $('form').find('#chapter').val();
+        const verse = $('form').find('#verse').val();
+        const reflection = $('form').find('#topic').val();
+        /* Need to know how to pass in radio button values*/
+
+        $.ajax({
+            dataType: 'json',
+            method: "POST",
+            url: USERS_URL,
+            contentType: "application/json",
+            data: JSON.stringify({
+                topic: topic,
+                //how to pass in id of user
+                passage: {
+                    book: book,
+                    chapter: chapter,
+                    verse: verse,
+                },
+                reflection: reflection,
+                //do I need to change to a boolean 
+                visibility: "true",
+            }),
+            error: function (a, b, c) {
+                console.log(a, b, c)
+            },
+            success: function (data) {
+                console.log(data);
+            }
+        })
+
+
+    })
+
+}
+
+
+
+function getInputValue(destination) {
     $('form').find(destination).val();
     console.log(destination);
 }
 
 
 
-function displayNotes(data){
+function displayNotes(data) {
     $('.js_username').html(data.notes[0].userName);
 
-    for(let i =0; i < data.notes.length; i++){
+    for (let i = 0; i < data.notes.length; i++) {
         $('.js_rows').append(`
         <tr>
             <td><a href="">${data.notes[i].topic}</a></td>
@@ -115,25 +191,25 @@ function displayNotes(data){
             <td><button>Delete</button></td>
         </tr>
 
-    `)};
+    `)
+    };
 
     // $('.js_rows').append()
 }
 
 
-function getNotesandDisplay(){
+function getNotesandDisplay() {
 
     getNotes(displayNotes);
     console.log('this ran');
 }
 
-$(function() {
+$(function () {
     getNotesandDisplay();
     logintoSite();
     // $.get('http://localhost:8080/', function(data){
     //     console.log(data);
     // });
+    signupForSite();
     $('#enter').click();
 })
-
-
